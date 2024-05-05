923f8380151ff66bc88ae0978748b81ae44e1a09
developerXDhanjit <developerdhanjit@gmail.com>
2024-05-05 18:56:12 +0530

    Found a way to actually fetch data

--- dist/JS/mainpage.js
+++ dist/JS/mainpage.js
@@ -18,5 +18,13 @@ let searchAnimeButton = document.querySelector("#search-icon");
 
 searchAnimeButton.addEventListener("click", (e) => {
   let searchAnimeInputValue = searchAnimeInput.value;
-  window.open("./searchResultsPage.html")
+  window.open("./searchResultsPage.html");
 });
+
+async function fetchAnime(nameOfAnime) {
+  let response = await fetch(`https://api.jikan.moe/v4/anime?q=${nameOfAnime}`);
+  let data = await response.json()
+  console.log(data.data);
+}
+
+fetchAnime("Naruto")
